1363a8b6b70b428b02e0c0d0def8dcd60b7098d8
Rachel Schipull <rlschipull@gmail.com>
2023-05-16 15:13:30 -0400

    added spotify node module and session code

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,6 +5,7 @@ import {
   type DefaultSession,
 } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import SpotifyWebApi from "spotify-web-api-node";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -30,6 +31,29 @@ declare module "next-auth" {
   // }
 }
 
+// async function refreshAccessToken(token) {
+//   try {
+//     spotifyApi.setAccessToken(token.accessToken);
+//     spotifyApi.setRefreshToken(token.refreshToken);
+
+//     const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+//     console.log("REFRESHED TOKEN", refreshedToken);
+
+//     return {
+//       ...token,
+//       accessToken: refreshedToken.access_token,
+//       accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000, // 1 hour as 3600
+//       refreshToken: refreshedToken.refresh_token ?? token.refreshToken, // if refresh token is not returned, keep the old one
+//     };
+//   } catch (err) {
+//     console.error(err);
+//     return {
+//       ...token,
+//       error: "Refresh token failed",
+//     };
+//   }
+// }
+
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
  *
@@ -37,13 +61,38 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    // session: ({ session, user }) => ({
+    //   ...session,
+    //   user: {
+    //     ...session.user,
+    //     id: user.id,
+    //   },
+    // }),
+    jwt: ({ token, account, user }) => {
+      // If initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+      // Return previous token if access token is still valid
+      if (token && token.accessTokenExpires > Date.now()) {
+        console.log("EXISTING TOKEN IS VALID");
+        return token;
+      }
+      // Refresh token if token is expired
+      console.log("ACCESS TOKEN EXPIRED, REFRESHING...");
+      return await refreshAccessToken(token);
+    },
+    session: ({ session, token }) => {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+    },
   },
   adapter: PrismaAdapter(prisma),
   providers: [
