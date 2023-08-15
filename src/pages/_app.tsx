b8892d7716ac8c39ff04aaeb89abb94fa9e5b376
Rachel Schipull <rlschipull@gmail.com>
2023-08-15 15:19:03 -0400

    hopefully nothing major

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+//import { ClerkProvider } from '@clerk/nextjs'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -18,3 +19,4 @@ const MyApp: AppType<{ session: Session | null }> = ({
 };
 
 export default api.withTRPC(MyApp);
+
